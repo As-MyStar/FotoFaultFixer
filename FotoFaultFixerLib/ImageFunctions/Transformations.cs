0ff96d3dab15a74bfacda4e37bbc8fcbadf0f15b
FiddlyDigital <fiddlydigital@gmail.com>
2020-12-17 13:46:49 +0000

    Implemented Vertical and Horizontal flipping Transformations

--- FotoFaultFixerLib/ImageFunctions/Transformations.cs
+++ FotoFaultFixerLib/ImageFunctions/Transformations.cs
@@ -12,7 +12,24 @@ namespace FotoFaultFixerLib.ImageFunctions
                 throw new ArgumentNullException();
             }
 
-            throw new NotImplementedException();
+            CImage verticallyFlipped = new CImage(original.Width, original.Height, original.NBits);
+
+            int i_orig;
+            int i_flipped;
+            int maxY = original.Height - 1;
+            for (int y = 0; y < original.Height; y++)
+            {
+                for (int x = 0; x< original.Width; x++)
+                {
+                    i_orig = (x + (original.Width * y));
+                    i_flipped = (x + (original.Width * (maxY - y)));
+                    verticallyFlipped.Grid[0 + 3 * i_flipped] = original.Grid[0 + 3 * i_orig];
+                    verticallyFlipped.Grid[1 + 3 * i_flipped] = original.Grid[1 + 3 * i_orig];
+                    verticallyFlipped.Grid[2 + 3 * i_flipped] = original.Grid[2 + 3 * i_orig];                    
+                }
+            }
+
+            return verticallyFlipped;
         }
 
         public static CImage FlipHorizontal(CImage original)
@@ -22,7 +39,24 @@ namespace FotoFaultFixerLib.ImageFunctions
                 throw new ArgumentNullException();
             }
 
-            throw new NotImplementedException();
+            CImage horizontallyFlipped = new CImage(original.Width, original.Height, original.NBits);
+
+            int i_orig;
+            int i_flipped;
+            int maxX = original.Width - 1;
+            for (int y = 0; y < original.Height; y++)
+            {
+                for (int x = 0; x < original.Width; x++)
+                {
+                    i_orig = (x + (original.Width * y));
+                    i_flipped = ((maxX - x) + (original.Width * y));
+                    horizontallyFlipped.Grid[0 + 3 * i_flipped] = original.Grid[0 + 3 * i_orig];
+                    horizontallyFlipped.Grid[1 + 3 * i_flipped] = original.Grid[1 + 3 * i_orig];
+                    horizontallyFlipped.Grid[2 + 3 * i_flipped] = original.Grid[2 + 3 * i_orig];
+                }
+            }
+
+            return horizontallyFlipped;
         }
 
         public static CImage RotateCW(CImage original)
